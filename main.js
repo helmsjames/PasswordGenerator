e14d0b9578f30ca6ccb3cac23696fd127eaa65b8
James Helms <helms.james@gmail.com>
2020-01-28 11:26:08 -0600

    updating final working code

--- main.js
+++ main.js
@@ -6,40 +6,69 @@ var numbers;
 
 
 function writePassword() {
-    var password = generatePassword();
+    var length = prompt('select at least 8 up to and including 128 and no more than 128');
+    if(isNaN(parseInt(length)) || length < 8 || length > 128) {
+        //We have bad input
+        alert('You did not give us good input');
+        return;
+    }
+
+    //Here we actually have a valid number
+    var upperChars = confirm('Would you like to include: Uppercase y/n');
+    var lowerChars = confirm('Would you like to include: Lowercase y/n');
+    var symbols = confirm('Would you like to include: Symbols y/n');
+    var numbers = confirm('Would you like to include: Numbers y/n'); 
+    console.log(upperChars, lowerChars, symbols, numbers)
+
+    //Right here if we make it this far.....WE shold kow a correct length and all the chars needed
+    var characterSetArray = generateCharacterSetArray(upperChars, lowerChars, symbols, numbers);
+    var password = generatePassword(length, characterSetArray);
+    console.log(password);
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
 }
 
 generateBtn.addEventListener('click', writePassword);
+//['0123456789', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'] => '0123456789'.split() => [0, 1, 2, 3,...]
+function generatePassword(len, characterArray){
+   //We need to choose a random TYPE first
+   var password =  '';
+   while(password.length !== parseInt(len)) {
+    var characterSet = characterArray[Math.floor(Math.random() * characterArray.length)]
 
-function generatePassword(){
-    //
-definteCriteraForPassword()
-    //
-
-    //
-
-    //
+   //Then from the random type choose a random char
+   var char = characterSet[Math.floor(Math.random() * characterSet.length)]
+    password+=char;
+  
+}
+return password;
 }
 
-function definteCriteraForPassword(){
-
+function generateCharacterSetArray(useUpper, useLower, useSymbols, useNumbers){
+    var results = [];
     //lower case chars
-    var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz".split("");
-    //we need to generate a random number to find a random character
-    var randomIndex = Math.floor(Math.random()*lowerCaseChars.length);
-
+    var lowerCaseChars = "abcdefghijklmnopqrstuvwxyz"
+    if(useLower) {
+        results.push(lowerCaseChars);
+    }
     //upper case chars
-    var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-
+    var upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if(useUpper) {
+        results.push(upperCaseChars);
+    }
     //special chars
-    var specialChars = "!@#$5^&*(){}[]:;,.<>".split("");
-
+    var specialChars = "!@#$5^&*(){}[]:;,.<>"
+    if(useSymbols) {
+        results.push(specialChars);
+    }
     //numbers
-    var numbers = "0123456789".split("");
+    var numbers = "0123456789"
+    if(useNumbers) {
+        results.push(numbers);
+    }
 
+    return results;
 }
 
 
